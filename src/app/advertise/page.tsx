a4d3d8d9c38e3c42c8f364f5bb3f29fbe458501f
Valeed Naeem <valeednaeem@gmail.com>
2025-03-05 16:48:13 +0500

    Updated page.tsx
    featureProps removed properly....

--- src/app/advertise/page.tsx
+++ src/app/advertise/page.tsx
@@ -1,88 +1,69 @@
-import React from 'react'
-import {
-  BatteryCharging,
-  GitPullRequest,
-  Layers,
-  RadioTower,
-  SquareKanban,
-  WandSparkles,
-} from "lucide-react"
-import { JSX } from 'react';
-interface FeatureProps {
-  heading: string;
-    reasons: {
-    title: string,
-    description: string,
-    icon: JSX.Element
-  }[]
-}
+  import React from 'react'
+  import {
+    BatteryCharging,
+    GitPullRequest,
+    Layers,
+    RadioTower,
+    SquareKanban,
+    WandSparkles,
+  } from "lucide-react"
 
-const FeatureProps = ({
-    heading : "Why Work With Us?",
-    reasons : [
-        {
-            title: "Quality",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?",
-            icon: <GitPullRequest className="size-6" />,
-        },
-        {
-            title: "Experience",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?",
-            icon: <SquareKanban className="size-6" />,
-        },
-        {
-            title: "Support",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?",
-            icon: <RadioTower className="size-6" />,
-        },
-        {
-            title: "Innovation",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?",
-            icon: <WandSparkles className="size-6" />,
-        },
-        {
-            title: "Results",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?",
-            icon: <Layers className="size-6" />,
-        },
-        {
-            title: "Efficiency",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?",
-            icon: <BatteryCharging className="size-6" />,
-        },
-    ],
-})
-
-function page(FeatureProps: FeatureProps) {
-    const { heading, reasons } = FeatureProps
-    return (
-    <section className="py-32">
-      <div className="container">
-        <div className="mb-10 md:mb-20">
-          <h2 className="mb-2 text-center text-3xl font-semibold lg:text-5xl">
-            {heading}
-          </h2>
-        </div>
-        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
-          {reasons.map((reason, i) => (
-            <div key={i} className="flex flex-col">
-              <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
-                {reason.icon}
+  function page() {
+      return (
+      <section className="py-32">
+        <div className="container">
+          <div className="mb-10 md:mb-20">
+            <h2 className="mb-2 text-center text-3xl font-semibold lg:text-5xl">
+              Why Work With Us
+            </h2>
+          </div>
+          <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+              <div className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  <GitPullRequest className="size-6" />
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">Quality</h3>
+                <p className="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?</p>
               </div>
-              <h3 className="mb-2 text-xl font-semibold">{reason.title}</h3>
-              <p className="text-muted-foreground">{reason.description}</p>
-            </div>
-          ))}
+              <div className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  <SquareKanban className="size-6" />
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">Experience</h3>
+                <p className="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?</p>
+              </div>
+              <div className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  <RadioTower className="size-6" />
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">Support</h3>
+                <p className="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?</p>
+              </div>
+              <div className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  <WandSparkles className="size-6" />
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">Innovation</h3>
+                <p className="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?</p>
+              </div>
+              <div className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  <Layers className="size-6" />
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">Results</h3>
+                <p className="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?</p>
+              </div>
+              <div className="flex flex-col">
+                <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                  <BatteryCharging className="size-6" />
+                </div>
+                <h3 className="mb-2 text-xl font-semibold">Efficiency</h3>
+                <p className="text-muted-foreground">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?</p>
+              </div>
+          </div>
         </div>
-      </div>
-    </section>
-  )
-}
+      </section>
+    )
+  }
 
-export default page
+  export default page
