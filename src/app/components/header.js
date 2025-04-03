5ad4dcf998945e6c80f9e6ab18b1e16a1ac3f3db
Valeed Naeem <valeednaeem@gmail.com>
2025-04-03 15:08:34 +0500

    Some updates in the files
    Some files are updated to remove the error.

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -13,9 +13,9 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Header() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-600">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
